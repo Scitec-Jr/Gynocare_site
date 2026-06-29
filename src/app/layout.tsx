05821a6108d563f7a0627a0420e3e0e47741c4a0
Vinícius Gonçalves <vinicius.devlp@gmail.com>
2026-06-29 01:59:38 -0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,28 +3,23 @@ import "./globals.css";
 import { inter, konkhmerSleokchher } from "../../public/fonts/fonts";
 
 export const metadata: Metadata = {
-    title: "Gynocare",
-    description: "Clínica especializada em exames e consultas",
-    keywords: ["ginecologia", "exames", "consultas", "ultrassom"],
-    authors: [
-        {
-            name: "Gynocare",
-        },
-    ],
+	title: "Gynocare",
+	icons: {
+		icon: "/favicon.ico",
+	},
+	description: "Clínica especializada em exames e consultas",
+	keywords: ["ginecologia", "exames", "consultas", "ultrassom"],
+	authors: [
+		{
+			name: "Gynocare",
+		},
+	],
 };
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-        <html
-            lang="pt"
-            className={`${inter.variable} ${konkhmerSleokchher.variable}`}
-            suppressHydrationWarning
-        >
-            <body>{children}</body>
-        </html>
-    );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="pt" className={`${inter.variable} ${konkhmerSleokchher.variable}`} suppressHydrationWarning>
+			<body>{children}</body>
+		</html>
+	);
 }
